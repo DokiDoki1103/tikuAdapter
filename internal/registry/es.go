ecb2c9a7d841710b8dd45642c957b2d5e61a7f1f
逆流而上 <1666888816@qq.com>
2023-11-26 03:04:23 +0800

    feat: 接入模糊搜索

--- internal/registry/es.go
+++ internal/registry/es.go
@@ -5,7 +5,7 @@ import (
 	"github.com/itihey/tikuAdapter/internal/service"
 )
 
-// Config 注册配置文件
+// RegisterEs 注册配置文件
 func RegisterEs(cfg configs.Config) service.Elasticsearch {
 	client, err := service.NewElasticsearchClient(cfg.Elasticsearch.Addresses)
 	if err != nil {
